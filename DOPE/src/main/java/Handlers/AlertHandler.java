3642ed47cb301ef26261e08144e1a2aa0071ea70
Gagong <belyaevroma@yandex.ru>
2020-01-05 17:57:12 +0300

    BotKey update

--- DOPE/src/main/java/Handlers/AlertHandler.java
+++ DOPE/src/main/java/Handlers/AlertHandler.java
@@ -41,7 +41,7 @@ public class AlertHandler extends TimerTask {
     private JDA jda;
     {
         try {
-            jda = new JDABuilder("NjA5Mzk3Mjg2NzU3NDY2MTMz.XhDisg.RPZwHLLQnypA2PTSDOIl5K1cubg").build().awaitReady();
+            jda = new JDABuilder("NjA5Mzk3Mjg2NzU3NDY2MTMz.XhH5Bw.KiuoThBYdBWswcuRqRPtckgFk0Y").build().awaitReady();
             // main - NjA5Mzk3Mjg2NzU3NDY2MTMz.XfEmZQ.W0qXjoc-MiyTC8xx8HaSYiKnmFY
             // test - NjM3NzE4NDcyNDAyNjY1NDcy.Xfpntw.rLJb4O-A_lUButzij_R7ez0GGVg
             Debug.p("AlertHandler", "JDA", "Finished Building 2 JDA!");
